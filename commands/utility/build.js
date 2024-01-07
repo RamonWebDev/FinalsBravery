0d9bc3411a8267a5b0f8df389d0ac57add3bd159
RamonWebDev <ramonplaysgames@gmail.com>
2024-01-06 22:39:05 -0600

    holds builds

---
+++ commands/utility/build.js
@@ -0,0 +1,20 @@
+const build = {
+    "Light": {
+      "Specializations": ["Cloaking Device", "Evasive Dash", "Grappling Hook"],
+      "Weapon": ["V9S", "XP-54", "M11", "Throwing Knives", "SH1900", "SR-84", "LH1", "Sword", "Dagger"],
+      "Gadgets": ["Breach Charge", "Flashbang", "Frag Grenade", "Gas Grenade", "Goo Grenade", "Pyro Grenade", "Thermal Vision", "Glitch Grenade", "Motion Sensor", "Smoke Grenade", "Stun Gun", "Vanishing Bomb"],
+      },
+    "Medium": {
+      "Specializations": ["Guardian Turret", "Healing Beam", "Recon Senses"],
+      "Weapon": ["AKM", "CL-40", "FCAR", "Model 1887", "R.357", "Riot Shield"],
+      "Gadgets": ["Flashbang", "Frag Grenade", "Gas Grenade", "Goo Grenade", "Pyro Grenade", "APS Turret", "Defibrillator", "Explosive Mine", "Gas Mine", "Glitch Trap", "Jump Pad","Sonar Grenade", "Tracking Dart", "Zipline"],
+    },
+    "Heavy": {
+      "Specializations": ["Charge'N'Slam", "Goo Gun", "Mesh Shield"],
+      "Weapon": ["Flamethrower", "Lewis Gun", "M32GL", "M60", "SA1216", "Sledgehammer"],
+      "Gadgets": ["Flashbang", "Frag Grenade", "Gas Grenade", "Goo Grenade", "Pyro Grenade", "Barricade", "C4", "Dome Shield", "Explosive Mine", "Pyro Mine", "RPG-7"],
+      },   
+
+    }
+
+    module.exports = build;
